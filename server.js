05686129145f2a323a38c7f8c8107e8da1556245
Simone Taeggi <startae14@gmail.com>
2020-06-06 00:37:50 +0200

    add route image API

--- server.js
+++ server.js
@@ -75,6 +75,23 @@ app.get('/profile/:id', (req, res) => {
   }
 });
 
+// image route
+app.put('/image', (req, res) => {
+  // grab id
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach( user => {
+    if(user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json('not found');
+  }
+})
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
