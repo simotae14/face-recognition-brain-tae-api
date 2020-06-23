9b4d2edbfc97d0b6a8626ebfd3fbed72b44cfcc6
simotae14 <startae14@gmail.com>
2020-06-23 21:27:02 +0200

    add connection config heroku

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
