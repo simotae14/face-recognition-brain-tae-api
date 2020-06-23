268edfdae37e759441f95777f34587177f997370
simotae14 <startae14@gmail.com>
2020-06-23 21:56:09 +0200

    config knex

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
+  client: 'pg',
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
